cfab700f70ad997b9a1452b69d7380691dd2ac67
Pushpit <pushpit07@gmail.com>
2021-12-16 11:44:53 +0530

    Script tag
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
 
 class MyDocument extends Document {
 	render() {
@@ -13,7 +12,6 @@ class MyDocument extends Document {
 				<body>
 					<Main />
 					<NextScript />
-					<Script src="https://kit.fontawesome.com/0366dd7992.js" crossorigin="anonymous"></Script>
 				</body>
 			</Html>
 		);
