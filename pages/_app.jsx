2bc03ec2a4ebdc637f24eaa3744d275271c90857
Pushpit <pushpit07@gmail.com>
2021-12-19 21:54:35 +0530

    Refactored code and added navbar
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,20 @@
 import "../styles/globals.css";
 import { MoralisProvider } from "react-moralis";
-import Script from 'next/script'
+import Script from 'next/script';
+import Layout from '../pageComponents/Layout/Layout';
 
 const MORALIS_APP_ID = "3Fg7xGs8RJcm6hqploLq21zyTJuivkud3yHrYqmZ";
 const MORALIS_SERVER_URL = "https://vc8ylcqwpen1.usemoralis.com:2053/server";
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Script src="https://kit.fontawesome.com/0366dd7992.js" crossorigin="anonymous"></Script>
 			<MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
-				<Component {...pageProps} />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
 			</MoralisProvider>
 		</>
 	);
 }
-
-export default MyApp;
