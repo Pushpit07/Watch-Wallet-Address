ab728eb3328e7e2ecd98f3ff704d981b5856ced0
Pushpit <pushpit07@gmail.com>
2021-12-31 18:32:12 +0530

    Used env variables
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,12 +4,11 @@ import { MoralisProvider } from "react-moralis";
 import Script from "next/script";
 import Layout from "../pageComponents/Layout/Layout";
 
-const MORALIS_APP_ID = "3Fg7xGs8RJcm6hqploLq21zyTJuivkud3yHrYqmZ";
-const MORALIS_SERVER_URL = "https://vc8ylcqwpen1.usemoralis.com:2053/server";
-
 export default function App({ Component, pageProps }) {
     // const [account, setAccount] = useState("0x48b7cedf1d50ce6595a027c9234d5e5bef54e09c");
     const [account, setAccount] = useState("0x54e006260e0ba68178c4280bff08e9ec3e81f5a7");
+    const MORALIS_APP_ID = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
+    const MORALIS_SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
 
     return (
         <>
